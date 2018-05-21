cd222516ef66cec0e08aa990bc06698340061b95
cdplourde <cdplourde@gmail.com>
2018-05-21 16:47:50 -0700

    geocoder

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,6 +6,23 @@ $(document).ready(function () {
 });
 
 
+// can we use this???
+// function getLatitudeLongitude(callback, address) {
+//   // If adress is not supplied, use default value 'Ferrol, Galicia, Spain'
+//   address = address || 'Ferrol, Galicia, Spain';
+//   // Initialize the Geocoder
+//   geocoder = new google.maps.Geocoder();
+//   if (geocoder) {
+//       geocoder.geocode({
+//           'address': address
+//       }, function (results, status) {
+//           if (status == google.maps.GeocoderStatus.OK) {
+//               callback(results[0]);
+//           }
+//       });
+//   }
+// }
+
 
 $("#username").focusout(function () {
 
